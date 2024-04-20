92eaaa28e20c80a680a0188365da857a9b80f1c9
Lee YongBeen <yongkong9876@gmail.com>
2024-04-20 21:35:38 +0900

    practice guide in lab

--- fastapi_practice/app/main.py
+++ fastapi_practice/app/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    q: str | None = None,
+    item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update(**item.model_dump())
+    return results
